b791866157b180fe570c354111daa043c6ccd75b
NAS89 <nicholasadam.strickland@gmail.com>
2016-11-01 19:28:46 -0700

    Create Config_Bot.py

---
+++ Config_Bot.py
@@ -0,0 +1,10 @@
+# This file contains the username and password to your bot.
+# DO NOT check it in!
+
+# Fill in the values and rename this file to config_bot.py
+
+REDDIT_USERNAME = 'PUTNAMEHERE'  # YOUR USERNAME as string
+REDDIT_PASS = 'PUTPASSHERE'  # YOUR PASSWORD as string
+SUBREDDIT = 'Panthers'
+TITLE = 'Game Thread: Arizona Cardinals (3-3-1) AT Carolina Panthers (1-5-0)'
+
